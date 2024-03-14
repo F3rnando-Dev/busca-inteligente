9fe376d4f4de33d1924e9122e35f4192dee0ed7d
Fernando Santana <fernando_92-@hotmail.com>
2024-03-14 17:28:06 -0300

    add: implementa modelagem simples de lojas

---
+++ busca-inteligente/src/busca/inteligente/model/LojaModel.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package busca.inteligente.model;
+
+/**
+ *
+ * @author ferna
+ */
+public class LojaModel {
+    
+    private int codigo;
+    private String descricao;
+    private long cnpj;
+    private String endereco;
+    private String cidade;
+
+    public LojaModel(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "LojaModel{" + "codigo=" + codigo + ", descricao=" + descricao + ", cnpj=" + cnpj + ", endereco=" + endereco + ", cidade=" + cidade + '}';
+    }
+}
