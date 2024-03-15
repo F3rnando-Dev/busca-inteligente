3f8e2f85e8078984eb53a88761de5f39235539dd
Fernando Santana <fernando_92-@hotmail.com>
2024-03-15 13:03:00 -0300

    BREAKING CHANGE: remove hifen do nome dos projetos e suas utilizacoes

--- busca-inteligente/src/buscainteligente/dao/LojasAdjacentesDao.java
+++ busca-inteligente/src/buscainteligente/dao/LojasAdjacentesDao.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package busca.inteligente.dao;
+package buscainteligente.dao;
 
-import busca.inteligente.model.LojasAdjacentesModel;
-import busca.inteligente.connection.ConnectionFactory;
+import buscainteligente.model.LojasAdjacentesModel;
+import buscainteligente.connection.ConnectionFactory;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
